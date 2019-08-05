1870dee13a94ba292699f083d707572c6be738c9
kdaher <kayssar.daher@polyconseil.fr>
2019-08-05 14:34:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# AD-Password-Auditor
+
+AD-Password-Auditor is Powershell tool to quickly evaluate password security for Active Directory accounts. It is wrapper script for [DSInternals](https://github.com/MichaelGrafnetter/DSInternals). It performs a dictionary attack on the stored password hashes, and outputs a file with the cracked login/password combinations. 
+
+## Getting started
+
+AD-Password-Auditor is a simple Powershell wrapper script. To launch it :
+
+    .\AD-Password-Auditor.ps1 -Server [dc name] -Domain [domain] -MaxThreads 2 -WordlistDirectory [directory path] -ResultDirectory [directory path]
+
+### Arguments
+
+ - **Server**
+     + Specifies the target computer for the operation. Enter a fully qualified domain name (FQDN), a NetBIOS name, or an IP address. When the remote computer is in a different domain than the local computer, the fully qualified domain name is required.
+ - **Domain**
+     + Speficies the target domain for the operation. Enter the AD domain in DN format. Eg: `DC=mycompany,DC=group`
+ - **MaxThreads**
+     + Sets the maximum number of concurrent jobs to be run. Allowed values are ints between 1 and 10. It is recommended to enter a value smaller than the number of logical cores your machine has.
+ - **WordlistDirectory**
+     + The directory where the wordlist dictionaries are stored.
+ - **ResultDirectory**
+     + The directory where AD-Password-Auditor will output its results and the consolidated results.
+
+
+## Requirements
+
+### DSInternals
+
+[DSInternals](https://github.com/MichaelGrafnetter/DSInternals) is Powershell module providing cmdlets to evaluate password security.
+
+Install it on your machine by running the following command in an Powershell terminal with admin privileges:
+
+    Install-Module -Name DSInternals
+
+### Wordlists
+
+You can find wordlists anywhere on the Internet. You can also generate your own by using a tool such as [Mentalist](https://github.com/sc0tfree/mentalist).
+
+### User
+
+The script uses the privileges that the user launching it has on the AD. If you do not have an AD account, or the correct permissions, you will not be able to run the script successfully.
+
+## Performance
+
+AD-Password-Auditor only uses the CPU to process the hashes and wordlists. As a result, it is not as efficient as GPU-based evaluation methods. It is however much easier to deploy and run on any machine that can reach the targeted Active Directory.
+
+### Multithreading
+
+The script can launch multiple cracking jobs simultaneously. Every job runs on a single dictionary file. As such, it is recommended to provide multiple wordlist files and run more than one thread to accelerate the cracking job.
+
+Cracking is a CPU intensive task. For optimum results, it is recommended to launch the script with `-MaxThreads` set at `n-1` where `n` is the number of logical cores your machine has.
+
+### Wordlist size
+
+Wordlists are loaded in RAM. It is recommended to provide wordlists of 500Mb at most to avoid significant slowdowns.
---
+++ dev_script.ps1
@@ -0,0 +1,115 @@
+﻿Param(
+     [parameter(Mandatory=$true,HelpMessage="The Domain Controller to query")][string]$DC,
+     [parameter(Mandatory=$true,HelpMessage="The AD Domain to query")][string]$Domain,
+     [parameter(Mandatory=$true,HelpMessage="The maximum number of concurrent cracking threads to launch. It is recommended to provide a value below your CPU logical core count")][ValidateRange(1,10)][int]$MaxThreads,
+     [parameter(Mandatory=$true,HelpMessage="The directory containing the dictionary files to test out")][System.IO.FileInfo]$WordlistDirectory,
+     [parameter(Mandatory=$true,HelpMessage="The directory where the results will be output")][System.IO.FileInfo]$ResultDirectory
+)
+
+Write-Host "################################"
+Write-Host "#      AD-Password-Auditor     #"
+Write-Host "################################"
+
+#=============================================================
+# Validating input variables
+#=============================================================
+Set-Location (Split-Path $MyInvocation.MyCommand.Path)
+
+If (-Not (Test-Path $WordlistDirectory)) {
+    Write-Host "Wordlist directory does not exist. Please provide a valid directory. Exiting."
+    exit
+} ElseIf ( (Get-ChildItem $WordlistDirectory | Measure-Object).Count -eq 0 ) {
+    Write-Host "Wordlist directory is empty. Please provide a valid wordlist. Exiting."
+    exit
+}
+
+If (-Not (Test-Path $ResultDirectory)) {
+    Write-Host "Results directory does not exist. Creating it..."
+    New-Item $ResultDirectory -ItemType Directory
+} ElseIf ((Get-ChildItem $ResultDirectory | Measure-Object).Count -gt 0 ) {
+    Write-Host "Results directory is not emplty. Please delete all files before proceding."
+    exit
+}
+
+[string] $WordlistDirectory = (Resolve-Path $WordlistDirectory)
+[string] $ResultDirectory = (Resolve-Path $ResultDirectory)
+
+If (($MaxThreads -le 0) -or ($MaxThreads -ge 11) -or (-Not $MaxThreads -is [int])) {
+    Write-Host "The provided maximum thread count is not valid. Please provide an integer between 1 and 10. Exiting."
+    exit
+}
+
+#=============================================================
+# Cracking
+#=============================================================
+$ExecutionStartTime = Get-Date
+
+$ScriptBlock = {
+    Param (
+        [System.Object] [Parameter(Mandatory=$true)] $_,
+        [string] [Parameter(Mandatory=$true)] $WordlistDirectory,
+        [string] [Parameter(Mandatory=$true)] $ResultDirectory,
+        [string] [Parameter(Mandatory=$true)] $DC,
+        [string] [Parameter(Mandatory=$true)] $Domain
+    )
+
+    Set-Location $WordlistDirectory
+
+    $DC_jobvar = $DC.Clone()
+    $Domain_jobvar = $Domain.Clone()
+
+    $OutputFile = New-Item -Path $ResultDirectory -Name $_"_crack_result.txt"
+    $Dict = Get-Content $_ |  Select-String -Pattern "^\s*$" -NotMatch | ConvertTo-NTHashDictionary
+    # ADReplica must be fetched within the scriptblock, it cannot be passed as a parameter or script crashes
+    $ADReplica = Get-ADReplAccount -All -Server $DC_jobvar -NamingContext $Domain_jobvar
+    $ADReplica | Test-PasswordQuality -WeakPasswordHashes $Dict -ShowPlainTextPasswords | Out-File -FilePath $Outputfile
+
+    Write-Host "`n@" (Get-Date -DisplayHint Time) "- Finished cracking job for wordlist:" $_
+}
+
+Get-ChildItem $WordlistDirectory | ForEach-Object {
+    Write-Host "`n@" (Get-Date -DisplayHint Time) "- Launching cracking job for wordlist:" $_
+
+    Start-Job -ScriptBlock $ScriptBlock -ArgumentList ($_, $WordlistDirectory, $ResultDirectory, $DC, $Domain)
+
+    # Handle job execution
+    While($(Get-Job -State Running).Count -ge $MaxThreads) {
+        Get-Job | Wait-Job -Any | Out-Null
+    }
+    Get-Job -State Completed | % {
+        Receive-Job $_ -AutoRemoveJob -Wait
+    }
+}
+
+# Handle job execution
+While ($(Get-Job -State Running).Count -gt 0) {
+   Get-Job | Wait-Job -Any | Out-Null
+}
+Get-Job -State Completed | % {
+   Receive-Job $_ -AutoRemoveJob -Wait
+}
+
+If ((Get-ChildItem $ResultDirectory | Measure-Object).Count -gt 0 ) {
+    # Result directory is not empty. We can assume everything ran correctly.
+    $ts = (Get-Date) - $ExecutionStartTime
+    Write-Host ("`nTotal exuction time = {0:dd} Days {0:hh} Hours {0:mm} Minutes {0:ss} Seconds" -f $ts)
+}
+#=============================================================
+# Consolidating results in a single file
+#=============================================================
+$CrackedPasswords = ""
+Set-Location $ResultDirectory
+Get-ChildItem | ForEach-Object {
+    $RawCrackedPasswords = Get-Content -Raw $_ | Select-String -Pattern "Passwords of these accounts have been found in the dictionary:\r\n(.+\r\n)+Historical " -AllMatches | Foreach {$_.Matches} | Foreach {$_.Value} 
+    $CrackedPasswords += $RawCrackedPasswords -split "\r" | Select-String -Pattern "Historical","Passwords of these accounts have been found in the dictionary","^\s*$" -NotMatch
+}
+$OutputFilePath = ($ResultDirectory + "\CONSOLIDATED_CRACKED_CURRENT_PASSWORDS.txt")
+$CrackedPasswords.Split("`n").Trim() | Out-File -FilePath $OutputFilePath
+Set-Location (Split-Path $MyInvocation.MyCommand.Path)
+If (Test-Path $OutputFilePath) {
+    Write-Host "`nOutputted consolidated results to "$OutputFilePath
+}
+#=============================================================
+# Done. Exiting.
+#=============================================================
+Write-Host "Exiting AD Password Auditor."
